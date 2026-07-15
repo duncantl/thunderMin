80ce8ad1708f599b2603efa4092352268d1d07a2
Duncan Temple Lang <duncan@r-project.org>
2026-07-15 14:46:14 -0700

    convert time to POSIXct. Also allow the caller to get the text of the URL without converting it to a data.frame

--- R/getSoundingData.R
+++ R/getSoundingData.R
@@ -7,11 +7,18 @@ if(FALSE) {
 }
 
 getSoundingData =
-function(station, date)
-{    
-   u =sprintf("https://weather.uwyo.edu/wsgi/sounding?datetime=%s%%2000:00:00&id=%s&type=TEXT:CSV&src=BUFR",
-              date, station)
+function(station, date, asCSV = FALSE)
+{
+      
+   u = sprintf("https://weather.uwyo.edu/wsgi/sounding?datetime=%s%%2000:00:00&id=%s&type=TEXT:CSV&src=BUFR",
+               date, station)
 
-    txt = readLines(u)
-    read.csv(textConnection(txt))
+   txt = readLines(u)
+   if(asCSV)
+       return(txt)
+   
+   dt = read.csv(textConnection(txt))
+   dt$time = as.POSIXct(strptime(dt$time, "%Y-%m-%d %H:%M:%S"))
+   
+   dt
 }    
